95d89d39b8d088bce9d2f08508e11e1e2a8feb5c
Marleyman876 <grant_garfield@yahoo.com>
2021-08-28 09:31:49 +0200

    modified tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -7,27 +7,6 @@ from django.urls import reverse
 
 class Snacks_Test(TestCase):
   
-  def test_home_page(self):
-    url = reverse('home')
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 200)
-    
-  def test_about_page(self):
-    url = reverse('about')
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 200)
-    
-  def test_homepage(self):
-    url = reverse('home')
-    response = self.client.get(url)
-    self.assertTemplateUsed(response, 'home.html')
-    self.assertTemplateUsed(response, 'base.html')
-    
-  def test_about_page_code(self):
-    url = reverse('home')
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 200)
-    
   def test_snack_page_status(self):
     url = reverse('snack_list')
     response = self.client.get(url)
@@ -38,3 +17,25 @@ class Snacks_Test(TestCase):
     response = self.client.get(url)
     self.assertTemplateUsed(response, 'snack_list.html')    
   
+  
+  # def test_home_page(self):
+  #   url = reverse('home')
+  #   response = self.client.get(url)
+  #   self.assertEqual(response.status_code, 200)
+    
+  # def test_about_page(self):
+  #   url = reverse('about')
+  #   response = self.client.get(url)
+  #   self.assertEqual(response.status_code, 200)
+    
+  # def test_homepage(self):
+  #   url = reverse('home')
+  #   response = self.client.get(url)
+  #   self.assertTemplateUsed(response, 'home.html')
+  #   self.assertTemplateUsed(response, 'base.html')
+    
+  # def test_about_page_code(self):
+  #   url = reverse('home')
+  #   response = self.client.get(url)
+  #   self.assertEqual(response.status_code, 200)
+    
